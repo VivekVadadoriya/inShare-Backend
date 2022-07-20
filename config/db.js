5d94cd1eec3db9b54232ac9451dfb53feade99d5
Vivek Vadadoriya <vadadoriyavivek5@gmail.com>
2022-07-20 15:00:44 +0530

    Inital

---
+++ config/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+function connectDB() {
+    // Database connection 🥳
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+            useNewUrlParser: true,
+            //useCreateIndex: true,
+            useUnifiedTopology: true,
+            //useFindAndModify: true
+        })
+        .then((con) => {
+            console.log("database connection success");
+        })
+        .catch((err) => {
+            console.log("database connection failure");
+            console.log(err);
+        });
+}
+
+module.exports = connectDB;
